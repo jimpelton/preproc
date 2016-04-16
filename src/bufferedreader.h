0a5df45d1a25694e8f10004fad75087aace2738b
Jim Pelton <jimpelton@u.boisestate.edu>
2016-04-16 16:54:37 -0600

    thread in bufferedreader.

--- src/bufferedreader.h
+++ src/bufferedreader.h
@@ -44,12 +44,14 @@ public:
   ///////////////////////////////////////////////////////////////////////////////
   bool open(const std::string &path);
 
+  bool start();
+  bool stop();
 
   ///////////////////////////////////////////////////////////////////////////////
   /// \brief Fill the buffer with data.
   /// \return The number of elements (not necessarily bytes) in the buffer.
   ///////////////////////////////////////////////////////////////////////////////
-  void fillBuffer();
+  static void fillBuffer(BufferedReader *);
 
 
   ///////////////////////////////////////////////////////////////////////////////
@@ -64,18 +66,22 @@ public:
   void reset();
 
 
-  ///////////////////////////////////////////////////////////////////////////////
-  /// \brief Return pointer to buffer.
-  ///////////////////////////////////////////////////////////////////////////////
-//  Ty *buffer_ptr() const { return m_buffer; }
+  using ThrustNormalIter = thrust::detail::normal_iterator<Ty>;
+  using BuffersPair = thrust::pair<ThrustNormalIter, ThrustNormalIter>;
 
   //////////////////////////////////////////////////////////////////////////////
   /// \brief Get a begin/end iterator pair of the next buffer segment.
   //////////////////////////////////////////////////////////////////////////////
-  auto
-  next() {
+  BuffersPair
+  next()  {
+    m_mutex.lock();
+
     auto rval = m_buffers.front();
     m_buffers.pop();
+
+    m_mutex.unlock();
+
+    m_cv.notify_all();
     return rval;
   }
 
@@ -85,22 +91,29 @@ public:
     return m_buffer.size() / m_nBufs;
   }
 
-private:
 
-  auto makePair(int next_empty) {
+private:   // Methods
+
+  BuffersPair
+  makePair(int next, std::streampos gofor) {
     return thrust::make_pair(
-        m_buffer.begin() + (next_empty * bufferSizeElements()),
-        m_buffer.begin() + ((next_empty * bufferSizeElements()) + bufferSizeElements())
+        m_buffer.begin() + (next * bufferSizeElements()),
+        m_buffer.begin() + ((next * bufferSizeElements()) + gofor)
     );
   }
+
+
+private:   // Data members
+
   thrust::host_vector<Ty> m_buffer;
-  std::queue<thrust::pair<
-      decltype(m_buffer.begin()), decltype(m_buffer.begin())>> m_buffers;
+
+  using BuffersQueue = std::queue<thrust::pair<decltype(m_buffer.begin()), decltype(m_buffer.begin())>>;
+  BuffersQueue m_buffers;
 
   int m_nBufs;
 
   size_t m_szBytesTotal;
-  size_t m_filePos;
+//  size_t m_filePos;
   std::string m_path;
   std::ifstream *m_is;
   std::atomic<bool> m_stopReading;
@@ -121,9 +134,11 @@ BufferedReader<Ty>::BufferedReader
   int nbuf
 )
 //  : m_buffer{ nullptr }
-  : m_szBytesTotal{ bufSize }
+  : m_buffer{ }
+  , m_buffers{ }
   , m_nBufs{ nbuf }
-  , m_filePos{ 0 }
+  , m_szBytesTotal{ bufSize }
+//  , m_filePos{ 0 }
   , m_path{ }
   , m_is{ nullptr }
 { }
@@ -143,6 +158,9 @@ template<typename Ty>
 bool
 BufferedReader<Ty>::open(const std::string &path)
 {
+  if (m_stopReading == true) {
+    return false;
+  }
   m_path = path;
   m_is = new std::ifstream();
   m_is->open(path, std::ios::binary);
@@ -155,27 +173,51 @@ BufferedReader<Ty>::open(const std::string &path)
   return false;
 }
 
+
+template<typename Ty>
+bool
+BufferedReader<Ty>::start()
+{
+  m_stopReading = false;
+  m_readThread(this->fillBuffer, this);
+  return true;
+}
+
+template<typename Ty>
+bool
+BufferedReader<Ty>::stop()
+{
+  m_stopReading = true;
+}
+
 ///////////////////////////////////////////////////////////////////////////////
+// static
 template<typename Ty>
 void
-BufferedReader<Ty>::fillBuffer()
+BufferedReader<Ty>::fillBuffer(BufferedReader *inst)
 {
-  Ty *h_buf{ m_buffer.data() };
+  Ty *h_buf{ inst->m_buffer.data() };
   int next_empty{ 0 };
+  size_t buffer_size_elements{ inst->bufferSizeElements() };
 
-  while (! m_stopReading) {
-    if (m_buffers.size() < m_nBufs) {
-      size_t offset{ next_empty * bufferSizeElements() };
-      h_buf = h_buf + offset;
-
-      size_t bytes_to_read{ bufferSizeElements() * sizeof(Ty) };
-      m_is->read(reinterpret_cast<char *>(h_buf), bytes_to_read);
-
-      std::streampos amount{ m_is->gcount() };
-      m_filePos += amount;
-      m_buffers.push(makePair(next_empty));
-      next_empty += 1;
+  while (! inst->m_stopReading) {
+    while (inst->m_buffers.size() >= inst->m_nBufs) {
+      inst->m_cv.wait(inst->m_buffersMutex);
     }
+
+    h_buf = h_buf + (next_empty * buffer_size_elements);
+
+    inst->m_is->read(reinterpret_cast<char *>(h_buf), buffer_size_elements * sizeof(Ty));
+
+    std::streampos amount{ inst->m_is->gcount() };
+//    inst->m_filePos += amount;
+
+    inst->m_mutex.lock();
+    inst->m_buffers.push(inst->makePair(next_empty, amount/sizeof(Ty)));
+    inst->m_mutex.unlock();
+
+    next_empty = (next_empty + 1) % inst->m_nBufs;
+
   }
 }
 
@@ -186,7 +228,7 @@ BufferedReader<Ty>::reset()
 {
   m_is->clear();
   m_is->seekg(0, std::ios::beg);
-  m_filePos = 0;
+//  m_filePos = 0;
 }
 
 } // namespace bd
