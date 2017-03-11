bc0cae627a1aa925efccc649a011cc3162edd610
Jim Pelton <jimpelton@u.boisestate.edu>
2017-03-10 19:29:24 -0700

    allocateBuffers()

--- resample/src/main.cpp
+++ resample/src/main.cpp
@@ -13,7 +13,40 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+namespace
+{
 
+template<class Ty>
+char *
+allocateEmptyBuffers(char *mem,
+                     bd::BlockingQueue<bd::Buffer<Ty> *> &empty,
+                     size_t nBuff,
+                     size_t lenBuff)
+{
+
+  //    size_t const sz_buf{ szMem / nBuff };
+  //    size_t const count{ sz_buf / sizeof(Ty) };
+  Ty *p{ reinterpret_cast<Ty *>(mem) };
+
+  for (size_t i{ 0 }; i < nBuff; ++i) {
+    bd::Buffer<Ty> *buf{ new bd::Buffer<Ty>(p, lenBuff) };
+    empty.push(buf);
+    p += lenBuff;
+  }
+
+  return reinterpret_cast<char *>(p);
+
+} // allocateEmptyBuffers()
+
+} // namespace
+
+
+template<class Ty>
+void
+start()
+{
+
+}
 
 int main(int argc, char const **argv)
 {
@@ -26,20 +59,21 @@ int main(int argc, char const **argv)
   //TODO: populate cmdopts if dat file given.
 
   std::ifstream inFile(cmdOpts.inFilePath, std::ios::binary);
-  if (! inFile.is_open()) {
-    bd::Err() << "Input file was not opened: " << cmdOpts.inFilePath;
+  if (!inFile.is_open()) {
+    bd::Err() << "Input file was not found: " << cmdOpts.inFilePath;
     return 1;
   }
-
+  inFile.close();
 
   std::ofstream outFile(cmdOpts.outFilePath, std::ios::binary);
-  if (! outFile.is_open()) {
-    bd::Err() << "Output file was not opened: " << cmdOpts.outFilePath << std::endl;
+  if (!outFile.is_open()) {
+    bd::Err() << "Output file could not be opened: " << cmdOpts.outFilePath << std::endl;
     return 1;
   }
+  outFile.close();
 
   std::unique_ptr<bd::IndexFile> indexFile{ nullptr };
-  if (! cmdOpts.indexFilePath.empty()) {
+  /*if (! cmdOpts.indexFilePath.empty()) {
 
     indexFile = bd::IndexFile::fromBinaryIndexFile(cmdOpts.indexFilePath);
 
@@ -50,10 +84,11 @@ int main(int argc, char const **argv)
 
 //    resample::runFromIndexFile();
 
-  } else if (!cmdOpts.datFilePath.empty()) {
+  } else*/
+  if (!cmdOpts.datFilePath.empty()) {
 
     // check given dat file exists
-    std::ifstream datFile(cmdOpts.datFilePath, std::ios::binary);
+    std::ifstream datFile(cmdOpts.datFilePath);
     if (!datFile.is_open()) {
       bd::Err() << "Dat file was not opened: " << cmdOpts.datFilePath << std::endl;
       return 1;
@@ -67,60 +102,49 @@ int main(int argc, char const **argv)
     cmdOpts.vol_dims[2] = dfd.rZ;
     cmdOpts.dataType = dfd.dataType;
 
+    indexFile->getVolume().voxelDims(
+        { cmdOpts.vol_dims[0], cmdOpts.vol_dims[1], cmdOpts.vol_dims[2] });
+    indexFile->getVolume().
 
-    size_t orig_c{ cmdOpts.vol_dims[0] }, new_c{ cmdOpts.new_vol_dims[0] }; // col
-    size_t orig_r{ cmdOpts.vol_dims[1] }, new_r{ cmdOpts.new_vol_dims[1] }; // row
-    size_t orig_s{ cmdOpts.vol_dims[2] }, new_s{ cmdOpts.new_vol_dims[2] }; // slab
+    switch(cmdOpts.dataType) {
+      case bd::DataType::UnsignedCharacter:
+        start<uint8_t>();
+        break;
+      case bd::DataType::UnsignedShort:
+        start<uint16_t>();
+        break;
+      case bd::DataType::Float:
+        start<float>();
+        break;
+    }
 
-    resample::Resample::go(dfd.dataType);
+
+    glm::u64vec3 dest_dims{ cmdOpts.new_vol_dims[0],
+                            cmdOpts.new_vol_dims[1],
+                            cmdOpts.new_vol_dims[2] };
+
+
+
+    char *mem{ new char[] };
+
+
+
+
+
+
+
+
+//    size_t orig_c{ cmdOpts.vol_dims[0] }, new_c{ cmdOpts.new_vol_dims[0] }; // col
+//    size_t orig_r{ cmdOpts.vol_dims[1] }, new_r{ cmdOpts.new_vol_dims[1] }; // row
+//    size_t orig_s{ cmdOpts.vol_dims[2] }, new_s{ cmdOpts.new_vol_dims[2] }; // slab
+
+//    resample::Resample::go(dfd.dataType);
 
 
     // read original data into memory
-    inFile.seekg(0, std::ios::end);
-    std::streampos fileSize{ inFile.tellg() };
-    inFile.seekg(0, std::ios::beg);
-    char *image{ new char[fileSize] };
-    inFile.read(image, fileSize);
-    inFile.close();
 
 
-    resample::Grid<unsigned char> grid{ orig_c, orig_r, orig_s,
-                                        reinterpret_cast<unsigned char*>(image) };
-
-    size_t slabSize{ new_c * new_r };
-    char *slab{ new char[slabSize] };
-    std::memset(slab, 0, slabSize);
-
-
-    for (size_t s{ 0 }; s < new_s; ++s) {
-      for (size_t r{ 0 }; r < new_r; r++) {
-        for (size_t c{ 0 }; c < new_c; c++) {
-
-          unsigned char ival{
-              grid.interpolate(
-                  { s / float(new_s), r / float(new_r), c / float(new_c) }) };
-
-          slab[c + r * new_c] = ival;
-
-        }
-      } // for r
-
-      outFile.write(slab, slabSize);
-      if (s % 10 == 0) {
-        std::cout << "\r Wrote slab: " << s << std::flush;
-      }
-
-      memset(slab, 0, slabSize);
-
-    } // for(s
-
-    std::cout << std::endl;
-
-    outFile.flush();
-    outFile.close();
-
-    delete[] image;
-    delete[] slab;
   }
 
   return 0;
+}
